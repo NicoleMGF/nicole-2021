edc5b1b7df78a267ed310579f2ad24a02401eb5c
Nicole MGF <10135076+NicoleMGF@users.noreply.github.com>
2021-11-23 16:02:51 -0500

    Add card

--- src/c360/Card/card.js
+++ src/c360/Card/card.js
@@ -1,31 +1,96 @@
-/**
- * This Class is built on top of Elix's "ReactiveElement"
- * base element. Think of this like React's Component class.
- *
- * Documentation on ReactiveElement can be found here:
- * https://component.kitchen/elix/ReactiveElement
- *
- * Also check out documentation on ReactiveMixin, to better
- * understand how component state is managed:
- * https://component.kitchen/elix/ReactiveMixin
- * (you can also see `src/sds/Button/button.js` for a state management example)
- */
-
 import ReactiveElement from 'elix/src/core/ReactiveElement'
-import { template } from 'elix/src/base/internal'
+import AriaRoleMixin from 'elix/src/base/AriaRoleMixin'
+import { template, defaultState, state, setState } from 'elix/src/base/internal'
+import { internal } from 'elix'
 import { templateFrom } from 'elix/src/core/htmlLiterals.js'
 
 import stylesheet from './card.css'
 
-class C360Card extends ReactiveElement {
+class C360Card extends AriaRoleMixin(ReactiveElement) {
+  get [defaultState]() {
+    return Object.assign(super[defaultState], {
+      image: '',
+      title: 'title',
+      content: 'text content',
+      buttonText: 'button text'
+    })
+  }
+
+  // Image gets/sets state
+  get image() {
+    return this[state].image
+  }
+
+  set image(image) {
+    this[setState]({ image })
+  }
+
+  // Title gets/sets state
+  get title() {
+    return this[state].title
+  }
+
+  set title(title) {
+    this[setState]({ title })
+  }
+
+  // Content gets/sets state
+  get content() {
+    return this[state].content
+  }
+
+  set content(content) {
+    this[setState]({ content })
+  }
+
+  // Button text gets/sets state
+  get cta() {
+    return this[state].cta
+  }
+
+  set cta(cta) {
+    this[setState]({ cta })
+  }
+
+  [internal.render](changed) {
+    super[internal.render](changed)
+
+    if (changed.image) {
+      this[internal.ids].image.src = this[state].image
+    }
+    if (changed.title) {
+      this[internal.ids].title.textContent = this[state].title
+    }
+    if (changed.content) {
+      this[internal.ids].content.textContent = this[state].content
+    }
+    if (changed.buttonText) {
+      this[internal.ids].cta.textContent = this[state].cta
+    }
+    if (this[internal.firstRender]) {
+      this[internal.ids].cta.addEventListener('click', () => {
+        const event = new CustomEvent('ctaclick', {
+          bubbles: true,
+          composed: true,
+          detail: { cta: this[state].cta }
+        })
+        this.dispatchEvent(event)
+      })
+    }
+  }
+
   get [template]() {
     return templateFrom.html`
-      <style>
-        ${stylesheet}
-      </style>
-
-      <div>[Your Card Component Template Goes Here]</div>
-    `
+       <style>
+         ${stylesheet}
+       </style>
+       <article class="card">
+         <c360-headline level="h2" size="small" class="title"><span id="title"></span></c360-headline>
+         <img class="image" id="image" src="" alt="" />
+         <p class="content" id="content"></p>
+         <c360-button variant="brand" id="cta"></c360-button>
+       </article>
+     `
   }
 }
 
--- src/c360/Card/card.stories.js
+++ src/c360/Card/card.stories.js
@@ -6,32 +6,22 @@ export default {
   parameters: {
     docs: {
       page: docs
+    },
+    actions: {
+      handles: ['ctaclick']
     }
   }
 }
 
-export const RFC = () => {
-  return fragmentFrom.html`
-    <h1>Card</h1>
-    <p>
-      You need to build this Card component from the specs
-      outlined in the project's README file. Add one or more
-      additional "Stories" (this function is a story) that
-      showcase your component, and use Storybook's addons, such as
-      the responsive viewport, or the AXE accessibility validator,
-      to ensure that your component code is bulletproof.
-    </p>
-    <p>
-      We have scaffolded out two components to assist with your
-      development - this one, which is intended to contain the contents
-      within the shaded 'card', as well as a 'CardGroup' component,
-      that containts the composed component's structure.
-    </p>
-  `
-}
-
 export const Base = () => {
   return fragmentFrom.html`
-    <c360-card />
+    <c360-card
+      id="card"
+      image="/demo/giraffe.jpg"
+      title="Contact us"
+      content="Call us at 1-800-667-6389, click on chat in the lower right, or let us call you back."
+      cta="REQUEST A CALL"
+    >
+    </c360-card>
   `
 }
